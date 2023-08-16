121a8401f8efdd75771dd916ef0095f3712a76fb
Ivan Santobuono <mecaluxivansantobuono@gmail.com>
2023-08-16 00:08:49 -0300

    adding visuals

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import Constants from 'expo-constants';
+import RepositoryList from './RepositoryList';
 
 const Main = () => {
   return (
     <View style={{ marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
-      <Text>Rate Repository Application</Text>
+      <RepositoryList />
     </View>
   );
 };
